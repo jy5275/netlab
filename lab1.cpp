8e84615f0dcefb4642b9001fdcf169a5edf3e67d
jy5275 <527507046@qq.com>
2019-10-31 21:13:31 +0800

    Remove unused comment

--- lab1.cpp
+++ lab1.cpp
@@ -8,46 +8,22 @@ extern void SendFRAMEPacket(unsigned char* pData, unsigned int len);
 #define WINDOW_SIZE_BACK_N_FRAME 4
 
 typedef enum {data, ack, nak} frame_kind;
-
 typedef struct frame_head {
 	frame_kind kind;
 	unsigned seq, ack;
 	char data[100];
 };
-
 struct frame{
 	frame_head head;
 	unsigned size;
 };
-
 struct FrameGo{
 	frame *pfrm;
 	unsigned sz;
 };
 
-//FrameGo *swin, *end, *l, *r;
 deque<FrameGo> swin, waiting;
 unsigned winsize = 1, counter, items;
-//const unsigned MAX = 128;
-
-/* Number of frames in the sliding window */
-// unsigned items(){
-// 	return r>=l ? r-l : MAX - (l - r);
-// }
-
-// FrameGo *winopen(){
-// 	FrameGo *ret = r;
-// 	r++;
-// 	if (r == end) r -= MAX;
-// 	return ret;
-// }
-
-// bool winclose(){
-// 	l++;
-// 	if (l < end) return true;
-// 	else if (l == end) { l -= MAX; return true; }
-// 	else return false;
-// }
 
 /*
 * 停等协议测试函数
@@ -72,12 +48,8 @@ int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, UINT8 message
 		*F.pfrm = *(frame*)pBuffer;
 		F.sz = bufferSize;
 		waiting.push_back(F);
-
 		if (swin.size() >= winsize) 		// Is there the window full?
 			break;		
-		// FrameGo *p = winopen();			// Open a window slot
-		// p->pfrm = *((frame*)pBuffer);	// Set the slot as pBuffer
-		// p->sz = bufferSize;
 
 		F = waiting.front(); 
 		waiting.pop_front();
@@ -98,21 +70,9 @@ int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, UINT8 message
 			}
 		}
 		// Just ignore incorrect ack numbers...
-
-		// if (mQue.size()!=0){
-		// 	s = mQue.front();
-		// 	if (ntohl(s.pfrm->head.seq) == ack){	// ack seq number OK
-		// 		mQue.pop_front();
-		// 		s = mQue.front();
-		// 		SendFRAMEPacket((char*)s.pfrm, s.sz);	// send frames again???
-		// 	}
-		// } else {
-		// 	sendWinFull = false;	// data buffer is empty
-		// }
 		break;
 
-    default:
-        break;
+    default: break;
     }
     return 0;
 }
