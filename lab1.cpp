56124e34b2425ad8a00aef0c4d444be54b79d97d
jy5275 <527507046@qq.com>
2019-11-02 14:00:34 +0800

    Finished!

--- lab1.cpp
+++ lab1.cpp
@@ -1,4 +1,5 @@
 #include "sysinclude.h"
+#include<iostream>
 #include<deque>
 using namespace std;
 
@@ -7,90 +8,153 @@ extern void SendFRAMEPacket(unsigned char* pData, unsigned int len);
 #define WINDOW_SIZE_STOP_WAIT 1
 #define WINDOW_SIZE_BACK_N_FRAME 4
 
-typedef enum {data, ack, nak} frame_kind;
+typedef enum { data, ack, nak } frame_kind;
 typedef struct frame_head {
 	frame_kind kind;
-	unsigned seq, ack;
-	char data[100];
+	unsigned int seq, ack;
+	unsigned char data[100];
 };
-struct frame{
+struct frame {
 	frame_head head;
-	unsigned size;
-};
-struct FrameGo{
-	frame *pfrm;
-	unsigned sz;
+	unsigned int size;
 };
 
-deque<FrameGo> swin, waiting;
-unsigned winsize = 1, counter, items;
+deque<frame> swin, waiting;
+int next2acked = 0;
 
 /*
-* åœç­‰åè®®æµ‹è¯•å‡½æ•°
+* Í£µÈĞ­Òé²âÊÔº¯Êı
 */
 int stud_slide_window_stop_and_wait(char *pBuffer, int bufferSize, UINT8 messageType) {
-    unsigned ack, num;
-	FrameGo F;
-	
-    switch (messageType) {
-    case MSG_TYPE_TIMEOUT:	// send the prepared seq
-		num = ntohl(*(unsigned*)pBuffer);
-		for (unsigned i = 0; i < swin.size(); i++){	// Iterate to find the frame to resend
-			if (swin[i].pfrm->head.seq == num){
-				SendFRAMEPacket((unsigned char*)(swin[i].pfrm), swin[i].sz);
+	unsigned ack, num;
+	frame F;
+
+	switch (messageType) {
+	case MSG_TYPE_TIMEOUT:	// send the prepared seq
+		num = *(unsigned*)pBuffer;
+		for (unsigned i = 0; i < swin.size(); i++) {	// Iterate to find the frame to resend
+			if (num == ntohl(swin[i].head.seq)) {
+				SendFRAMEPacket((unsigned char*)(&swin[i]), swin[i].size);
 				break;
 			}
-		}
-        break;
-
-    case MST_TYPE_SEND:		// Prepare a new frame
-		F.pfrm = malloc(sizeof(frame));
-		*F.pfrm = *(frame*)pBuffer;
-		F.sz = bufferSize;
+		} break;
+	case MSG_TYPE_SEND:		// Prepare a new frame
+		F = *(frame*)pBuffer;
+		F.size = bufferSize;
 		waiting.push_back(F);
-		if (swin.size() >= winsize) 		// Is there the window full?
-			break;		
-
-		F = waiting.front(); 
+		if (swin.size() >= WINDOW_SIZE_STOP_WAIT) 		// Is there the window full?
+			break;
+		F = waiting.front();
 		waiting.pop_front();
-		SendFRAMEPacket((unsigned char*)(F.pfrm), F.sz);
+		SendFRAMEPacket((unsigned char*)(&F), F.size);
 		swin.push_back(F);
 		break;
-
 	case MSG_TYPE_RECEIVE:	// Receive ack
-		ack = ntohl((frame*)pBuffer)->head.ack);
-
-		if (ack == swin.front().pfrm->head.seq){
+		ack = ntohl(((frame*)pBuffer)->head.ack);
+		num = ntohl(swin.front().head.seq);
+		while (ntohl(swin.front().head.seq) <= ack) {
 			swin.pop_front();
 			if (waiting.size() != 0) {
 				F = waiting.front();
 				waiting.pop_front();
-				SendFRAMEPacket((unsigned char*)(F.pfrm), F.sz);
+				SendFRAMEPacket((unsigned char*)(&F), F.size);
 				swin.push_back(F);
 			}
-		}
-		// Just ignore incorrect ack numbers...
+		} break;
+	default: break;
+	}
+	return 0;
+}
+
+
+/*
+* »ØÍËnÖ¡²âÊÔº¯Êı
+*/
+int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, UINT8 messageType) {
+	unsigned ack, num, tmp;
+	frame F;
+
+	switch (messageType) {
+	case MSG_TYPE_TIMEOUT:	// send all timeout frames after num
+		num = *(unsigned*)pBuffer;
+		for (unsigned i = 0; i < swin.size(); i++) {	// Iterate to find the error frame
+			SendFRAMEPacket((unsigned char*)(&swin[i]), swin[i].size);
+		} break;
+	case MSG_TYPE_SEND:		// Prepare a new frame
+		F = *(frame*)pBuffer;
+		F.size = bufferSize;
+		waiting.push_back(F);
+		if (swin.size() >= WINDOW_SIZE_BACK_N_FRAME) 		// Is the window full?
+			break;
+		F = waiting.front();
+		waiting.pop_front();
+		SendFRAMEPacket((unsigned char*)(&F), F.size);
+		swin.push_back(F);
 		break;
-
-    default: break;
-    }
-    return 0;
+	case MSG_TYPE_RECEIVE:	// Receive ack
+		ack = ntohl(((frame*)pBuffer)->head.ack);
+		num = ntohl(swin.front().head.seq);
+		while (ntohl(swin.front().head.seq) <= ack) {
+			swin.pop_front();
+			if (waiting.size() != 0) {
+				F = waiting.front();
+				waiting.pop_front();
+				SendFRAMEPacket((unsigned char*)(&F), F.size);
+				swin.push_back(F);
+			}
+		} break;
+	default: break;
+	}
+	return 0;
 }
 
 
 /*
-* å›é€€nå¸§æµ‹è¯•å‡½æ•°
+* Ñ¡ÔñĞÔÖØ´«²âÊÔº¯Êı
 */
-int stud_slide_window_back_n_frame(char *pBuffer, int bufferSize, UINT8 messageType)
-{
-return 0;
-}
+int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 messageType) {
+	unsigned ack, num, tmp;
+	frame F;
 
-
-/*
-* é€‰æ‹©æ€§é‡ä¼ æµ‹è¯•å‡½æ•°
-*/
-int stud_slide_window_choice_frame_resend(char *pBuffer, int bufferSize, UINT8 messageType)
-{
-return 0;
+	switch (messageType) {
+	case MSG_TYPE_SEND:		// Prepare a new frame
+		F = *(frame*)pBuffer;
+		F.size = bufferSize;
+		waiting.push_back(F);
+		if (swin.size() >= WINDOW_SIZE_BACK_N_FRAME) 		// Is the window full?
+			break;
+		F = waiting.front();
+		waiting.pop_front();
+		SendFRAMEPacket((unsigned char*)(&F), F.size);
+		swin.push_back(F);
+		break;
+	case MSG_TYPE_RECEIVE:	// Receive ack or nak
+		num = ntohl(((frame*)pBuffer)->head.kind);
+		if (ntohl(((frame*)pBuffer)->head.kind) == 1) {
+			ack = ntohl(((frame*)pBuffer)->head.ack);
+			num = ntohl(swin.front().head.seq);
+			while (ntohl(swin.front().head.seq) <= ack) {
+				swin.pop_front();
+				if (waiting.size() != 0) {
+					F = waiting.front();
+					waiting.pop_front();
+					SendFRAMEPacket((unsigned char*)(&F), F.size);
+					swin.push_back(F);
+				}
+			}
+		}
+		else {
+			ack = ntohl(((frame*)pBuffer)->head.ack);
+			for (unsigned i = 0; i < swin.size(); i++) {		// Find the frame to resend
+				tmp = ntohl(swin.front().head.seq);
+				if (ack == ntohl(swin[i].head.seq)) {
+					SendFRAMEPacket((unsigned char*)(&swin[i]), swin[i].size);
+					break;
+				}
+			}
+		}
+		break;
+	default: break;
+	}
+	return 0;
 }
